b028a65024ff76040ff94afb72cb009d19b84cfc
Smith06S <sarahelenasmith@.com>
2025-03-21 10:40:03 +0100

    open port 8080 with all the browsers

--- main.go
+++ main.go
@@ -1,8 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os/exec"
+	"runtime"
+)
+
+func openBrowser(url string) {
+	var err error
+	switch os := runtime.GOOS; os {
+	case "linux":
+		err = exec.Command("xdg-open", url).Start()
+	case "windows":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		fmt.Printf("Failed to open browser: %v\n", err)
+	}
+}
 
 func main() {
-	fmt.Println("Bonjour depuis mon application Go en Docker !")
-	fmt.Println("test")
+	// Répertoire contenant les fichiers statiques
+	fs := http.FileServer(http.Dir("./static"))
+
+	// Serveur des fichiers statiques (CSS, JS)
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Route pour afficher la page d'accueil
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Charger le template HTML depuis le répertoire "template"
+		tmpl, err := template.ParseFiles("./templates/index.html")
+		if err != nil {
+			http.Error(w, "Erreur lors du chargement du template", http.StatusInternalServerError)
+			return
+		}
+
+		// Exécuter le template et l'envoyer au client
+		err = tmpl.Execute(w, nil)
+		if err != nil {
+			http.Error(w, "Erreur lors de l'exécution du template", http.StatusInternalServerError)
+		}
+	})
+
+	// Lancer le serveur sur le port 8080
+	fmt.Println("Serveur lancé sur http://localhost:8080")
+	go openBrowser("http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Printf("Server failed to start: %v\n", err)
+	}
+
 }
---
+++ view/go.mod
@@ -0,0 +1,10 @@
+module Forum
+
+go 1.23.2
+
+require github.com/mattn/go-sqlite3 v1.14.24
+
+require (
+	github.com/satori/go.uuid v1.2.0 // indirect
+	golang.org/x/crypto v0.36.0 // indirect
+)

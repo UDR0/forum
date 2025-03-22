abd649760ef160874387b49251d0192db034efd9
UDR0 <125358365+UDR0@users.noreply.github.com>
2025-03-22 23:46:26 +0100

    page d'accueil bientot fini

--- static/script.js
+++ static/script.js
@@ -6,3 +6,15 @@ const navMenu = document.querySelector(".meta-ul"); // Le menu de navigation
 burgerMenu.addEventListener("click", () => {
     navMenu.classList.toggle("active"); // Ajouter/retirer la classe 'active'
 });
+
+document.querySelectorAll('.coeur-container').forEach(coeur => {
+    coeur.addEventListener('click', function () {
+        const img = this.querySelector('.coeur');
+        if (img.src.includes('coeur.png')) {
+            img.src = 'static/img/coeur_rouge.png'; // Remplace par le cœur rouge
+        } else {
+            img.src = 'static/img/coeur.png'; // Reviens au cœur normal
+        }
+    });
+});
+

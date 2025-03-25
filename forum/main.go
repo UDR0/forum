45cb06582c28261c99c911e88e49ee4a22511b5b
Willpns <pwill06am@gmail.com>
2025-03-25 12:00:58 +0100

    Création de compte fonctionnel

---
+++ forum/main.go
@@ -0,0 +1,119 @@
+/*package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	tmplPath := fmt.Sprintf("templates/%s.html", tmpl)
+	t, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		http.Error(w, "Erreur lors du chargement du template : "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := t.Execute(w, nil); err != nil {
+		http.Error(w, "Erreur lors de l'exécution du template : "+err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+
+	//fmt.Println(forum.GetImageURLFromDB())
+	//forum.WriteImageIntoDB()
+
+	// Servir les fichiers statiques (CSS, JS, images)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Route pour la page d'accueil
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/mytripy-non", http.StatusFound)
+	})
+
+	// Routes pour les pages HTML
+	http.HandleFunc("/mytripy-non", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "mytripy_non")
+	})
+
+	http.HandleFunc("/SeConnecter", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "SeConnecter")
+	})
+
+	http.HandleFunc("/CreerCompte", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "CreerCompte")
+	})
+
+	http.HandleFunc("/mot-de-passe-oublie", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "mot-de-passe-oublie")
+	})
+
+	// Démarrer le serveur
+	fmt.Println("Serveur lancé sur http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Erreur lors du lancement du serveur :", err)
+	}
+}
+*/
+
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"forum/Functions"
+	"net/http"
+)
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	tmplPath := fmt.Sprintf("templates/%s.html", tmpl)
+	t, err := template.ParseFiles(tmplPath)
+	if err != nil {
+		http.Error(w, "Erreur lors du chargement du template : "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if err := t.Execute(w, nil); err != nil {
+		http.Error(w, "Erreur lors de l'exécution du template : "+err.Error(), http.StatusInternalServerError)
+	}
+}
+
+func main() {
+	// Servir les fichiers statiques (CSS, JS, images)
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	// Route pour la page d'accueil
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/mytripy_non", http.StatusFound)
+	})
+
+	// Routes pour les pages HTML
+	http.HandleFunc("/mytripy_non", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "mytripy_non")
+	})
+
+	http.HandleFunc("/SeConnecter", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "SeConnecter")
+	})
+
+	http.HandleFunc("/CreerCompte", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == http.MethodPost {
+			forum.CreateUser(w, r)
+		} else {
+			renderTemplate(w, "CreerCompte")
+		}
+	})
+
+	http.HandleFunc("/mot_de_passe_oublie", func(w http.ResponseWriter, r *http.Request) {
+		renderTemplate(w, "mot_de_passe_oublie")
+	})
+
+	// Démarrer le serveur
+	fmt.Println("Serveur lancé sur http://localhost:8080")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		fmt.Println("Erreur lors du lancement du serveur :", err)
+	}
+}
